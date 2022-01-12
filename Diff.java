9bec80ce06def5100c2335ee7bd01b2b82b2223f
Paharlaya Basnet <paharlayabasnet@gmail.com>
2022-01-12 12:46:02 +0530

    modified Diff.java

--- Diff.java
+++ Diff.java
@@ -1,3 +1,9 @@
 public class Diff {
     
+    public static void main (String[] args){
+        int a = 10;
+        int b= 6;
+        System.out.println("The diffrence is :" + (a-b));
+
+    }
 }
